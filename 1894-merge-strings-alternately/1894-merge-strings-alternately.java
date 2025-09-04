398177e6241ec475aa9296ab89a506c3b5877743
KKireev <lilo662@mail.ru>
2025-09-04 14:53:14 +0300

    Time: 8 ms (5.07%), Space: 42.8 MB (17.67%) - LeetHub

---
+++ 1894-merge-strings-alternately/1894-merge-strings-alternately.java
@@ -0,0 +1,28 @@
+class Solution {
+    public String mergeAlternately(String word1, String word2) {
+        String result="";
+        if(word1.length()>=word2.length()){
+            for(int i =0; i<word1.length();i++){
+                if(i>word2.length()-1){
+                    result += word1.charAt(i);
+                }
+                else{
+                    result += word1.charAt(i);
+                    result += word2.charAt(i);
+                }
+            }
+        }
+        else{
+            for(int i =0; i<word2.length();i++){
+                if(i>word1.length()-1){
+                    result += word2.charAt(i);
+                }
+                else{
+                    result += word1.charAt(i);
+                    result += word2.charAt(i);
+                }
+            }
+        }
+        return result;
+    }
+}
